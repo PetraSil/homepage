ff0993fa0a2b115ccb4795a844802dca499cb87a
Petra <petra.silavuori@gmail.com>
2019-12-22 23:02:35 +0200

    Minor stuff

--- src/pages/work/Work.js
+++ src/pages/work/Work.js
@@ -3,6 +3,7 @@ import DefaultContainerFlex from '../../containers/DefaultContainerFlex';
 import '../pages.css';
 import './work.css';
 import Article from '../../components/article/Article';
+import RecentProject from './recentProject/RecentProject';
 
 const Work = () => {
   
@@ -12,10 +13,9 @@ const Work = () => {
       <>
         <h1 className="page-title">Timeline</h1>
         <Article title={"ALTEN"} metatitle={"29.9.2019 - Present"} text={"Some text about the workplace and the work done there plus some fluff crap. Some text about the workplace and the work done there plus some fluff crap."}/>
-        <Article title={"ALTEN"} metatitle={"29.9.2019 - 29.9.2019"} text={"Some text about the workplace and the work done there plus some fluff crap."}/>
+        <Article title={"ALTEN"} metatitle={"29.9.2019 - 29.9.2019"} text={"Some text about the workplace and the dsd sd sad sad sad sadwork done there plus some fluff crap."}/>
         <Article title={"ALTEN"} metatitle={"29.9.2019 - 29.9.2019"} text={"Some text about the workplace and the work done there plus some fluff crap. Some text about the workplace and the work done there plus some fluff crap."}/>
-        <Article title={"ALTEN"} metatitle={"29.9.2019 - 20.9.2019"} text={"Some text about the workplace and the work done there plus some fluff crap."}/>
-        <h1 className="page-title">Latest Project</h1>
+        <Article title={"ALTEN"} metatitle={"29.9.2019 - 20.9.2019"} text={"Some text about the asd asd asd asd asd asd as das dasd as workplace and the work done there plus some fluff crap."}/>
       </>
     }
     />
--- src/pages/work/recentProject/RecentProject.js
+++ src/pages/work/recentProject/RecentProject.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const RecentProject = () =>
-  <section>
-		<div></div>
-		<
+  <section className="work-recent-project">
+		<h2>PROJECT ONE</h2>
+		<p>Lorem and sdio sodfj dfj sodifj dsoifj soidfj soidfjssdfsdf sdf sd sd d  sf ds sads asd sadsdasd asdas asd asd asdfdsd</p>
+		<div className="project-image"></div>
   </section>
 ;
 
