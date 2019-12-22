6dcfb2d254df0eeb8840d07a52ecb08ad909bccf
Petra <petra.silavuori@gmail.com>
2019-12-22 12:43:58 +0200

    Added react spring library for route transitions and adjustments related to that.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home';
 import Work from './pages/Work';
 import Contact from './pages/Contact';
-import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, withRouter, Switch, Route, __RouterContext } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
+import AbsoluteWrapper from './containers/AbsoluteWrapper';
 
 const App = () => {
+  
+const { location } = useContext(__RouterContext);
+const pageTransitions = useTransition(location, location => location.pathname, {
+  config: {duration: 500},
+  from: { opacity: 0 },
+  enter: { opacity: 1 },
+  leave: { opacity: 0 }
+});
+
   return (
-    <Router>
       <>
         <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/work" component={Work} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
+        {pageTransitions.map(({item, props, key}) => (
+          <AbsoluteWrapper content={
+            <animated.div key={key} style={props}>
+              <Switch location={item}>
+                <Route path="/" exact component={Home} />
+                <Route path="/work" component={Work} />
+                <Route path="/contact" component={Contact} />
+              </Switch>
+            </animated.div>
+          } />
+        ))}
       </>
-    </Router>
   );
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/containers/AbsoluteWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AbsoluteWrapper = ({ content }) =>
+  <div className="absolute-wrapper">
+      {content}
+  </div>
+;
+
+export default AbsoluteWrapper;
