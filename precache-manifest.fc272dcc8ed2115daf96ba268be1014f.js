d31504bcdb7d8cec612c17728f845c2917896632
Petra <petra.silavuori@gmail.com>
2019-12-23 12:19:56 +0200

    Updates

---
+++ precache-manifest.fc272dcc8ed2115daf96ba268be1014f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42af8ad62b8bb2ad33303148d95184f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f8324cfd27df9052ac",
+    "url": "/static/css/main.99bea0b8.chunk.css"
+  },
+  {
+    "revision": "b176cb39261b1208e785",
+    "url": "/static/js/2.22b00476.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.22b00476.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10f8324cfd27df9052ac",
+    "url": "/static/js/main.919262e1.chunk.js"
+  },
+  {
+    "revision": "1638115bd0cc75b337fe",
+    "url": "/static/js/runtime-main.0daf9c3b.js"
+  },
+  {
+    "revision": "d0b4528a99de1220946640eb1b934cf3",
+    "url": "/static/media/github.d0b4528a.png"
+  },
+  {
+    "revision": "34c68c57b8cedbf66c2ea14becfc3ff8",
+    "url": "/static/media/linkedin.34c68c57.png"
+  }
+]);
