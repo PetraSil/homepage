d329708eb6c9f8b3e93df851156aabed11266292
Petra <petra.silavuori@gmail.com>
2019-12-25 12:27:15 +0200

    Trying a workaround for a router problem with github hosting.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.render(
-  <Router>
+  <HashRouter basename="/">
     <App />
-  </Router>, 
+  </HashRouter>, 
   document.getElementById('root')
 );
