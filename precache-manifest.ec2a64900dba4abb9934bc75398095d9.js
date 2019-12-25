674ad36f714e0e8c554e03ad906f27a8b2002c94
Petra <petra.silavuori@gmail.com>
2019-12-25 21:29:25 +0200

    Updates

--- precache-manifest.ec2a64900dba4abb9934bc75398095d9.js
+++ precache-manifest.ec2a64900dba4abb9934bc75398095d9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "81c17df6ace3af7f44140a95d4727ad7",
+    "revision": "52e48650187762138e2ebbd3cd4c793b",
     "url": "/home/index.html"
   },
   {
-    "revision": "0bafd07ee434f4ca99e4",
-    "url": "/home/static/css/main.dd46c362.chunk.css"
+    "revision": "9eff91bed9e6821dd1f6",
+    "url": "/home/static/css/main.99294a81.chunk.css"
   },
   {
-    "revision": "7e97c687bea58a49ece6",
-    "url": "/home/static/js/2.16daae11.chunk.js"
+    "revision": "4633b5167c2859d04d96",
+    "url": "/home/static/js/2.600ee661.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/home/static/js/2.16daae11.chunk.js.LICENSE"
+    "url": "/home/static/js/2.600ee661.chunk.js.LICENSE"
   },
   {
-    "revision": "0bafd07ee434f4ca99e4",
-    "url": "/home/static/js/main.4b7f7500.chunk.js"
+    "revision": "9eff91bed9e6821dd1f6",
+    "url": "/home/static/js/main.ca9dd086.chunk.js"
   },
   {
     "revision": "8b3437c1db706c34df92",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.dea84679f7ef92b3f3779c423677a341.js"
+  "/home/precache-manifest.ec2a64900dba4abb9934bc75398095d9.js"
 );
 
 self.addEventListener('message', (event) => {
