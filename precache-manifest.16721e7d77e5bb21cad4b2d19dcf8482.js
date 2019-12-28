1476eab780dac103773a472e8afea2d239a22e4c
Petra <petra.silavuori@gmail.com>
2019-12-28 23:30:29 +0200

    Updates

--- precache-manifest.16721e7d77e5bb21cad4b2d19dcf8482.js
+++ precache-manifest.16721e7d77e5bb21cad4b2d19dcf8482.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "46a5f209e14797d6c80f747f6fe277c0",
+    "revision": "5861e9c17a21215fe80e506c9f46c37d",
     "url": "/home/index.html"
   },
   {
-    "revision": "d23961685dac87a20962",
-    "url": "/home/static/css/main.8dc27f6b.chunk.css"
+    "revision": "d38a8d728c4aede067ce",
+    "url": "/home/static/css/main.82e22b97.chunk.css"
   },
   {
     "revision": "4633b5167c2859d04d96",
@@ -16,19 +16,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.600ee661.chunk.js.LICENSE"
   },
   {
-    "revision": "d23961685dac87a20962",
-    "url": "/home/static/js/main.bbd6eeeb.chunk.js"
+    "revision": "d38a8d728c4aede067ce",
+    "url": "/home/static/js/main.26bbefea.chunk.js"
   },
   {
     "revision": "8b3437c1db706c34df92",
     "url": "/home/static/js/runtime-main.27f4abb7.js"
   },
   {
-    "revision": "d0b4528a99de1220946640eb1b934cf3",
-    "url": "/home/static/media/github.d0b4528a.png"
-  },
-  {
     "revision": "34c68c57b8cedbf66c2ea14becfc3ff8",
     "url": "/home/static/media/linkedin.34c68c57.png"
+  },
+  {
+    "revision": "abb0ddebd3aca5602ff893385cb5b191",
+    "url": "/home/static/media/myraL.abb0ddeb.PNG"
+  },
+  {
+    "revision": "94998f9826a18d09ddb9b75c87699f81",
+    "url": "/home/static/media/myraS.94998f98.PNG"
   }
 ]);
