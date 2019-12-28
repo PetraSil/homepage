c46dd76a660f7f08827627a0f6783296bba67483
Petra <petra.silavuori@gmail.com>
2019-12-28 23:32:25 +0200

    Fixed a typo.

--- src/pages/recentProject/RecentProject.js
+++ src/pages/recentProject/RecentProject.js
@@ -18,7 +18,7 @@ const RecentProject = () => {
 					<div className="project__content">
 						<p>MYRA aims to fill the need for a simple and streamlined way for a nurse to report their patient work at home care, which will save time and resource in a field that is in dire need of all the help it can get.<br/>
 							<br/>While still a work in progress, I wish to include it here despite not being ready, as some might find the process of building an application interesting. The architecture so far includes API design made for a Node.js backend that has been connected to a cloud based MongoDB database with the help of Express and Mongoose.
-							Front is being built using the latest React and Redux for state management based in my UI and UX designs.
+							Front is being built using the latest React and Redux for state management based on my UI and UX designs.
 							<br/><br/>
 							<a className="project__content_link" href="https://www.figma.com/proto/tbUEgtBiydUUPjhHWrFJyp/PatientApp?node-id=1%3A59&scaling=contain" target="_blank">FIGMA Design in Progress &#8594;</a> 
 							<a className="project__content_link" href="https://github.com/PetraSil/patientRegistrationApp" target="_blank">GITHUB Codebase in Progress &#8594;</a> 
