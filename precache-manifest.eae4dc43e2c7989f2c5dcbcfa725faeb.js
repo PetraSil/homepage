8be720c831aa40988582636326e1e17d7876d857
Petra <petra.silavuori@gmail.com>
2020-01-19 11:08:57 +0200

    Updates

---
+++ precache-manifest.eae4dc43e2c7989f2c5dcbcfa725faeb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f89e6e06c246d882cc5bf0df0694514",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "50b7e59dca01c5d06712",
+    "url": "/home/static/css/3.887825ac.chunk.css"
+  },
+  {
+    "revision": "9c40c554372b3cc5588a",
+    "url": "/home/static/css/main.e95b08d6.chunk.css"
+  },
+  {
+    "revision": "4714f7384dedff749c3b",
+    "url": "/home/static/js/2.03e790a3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/home/static/js/2.03e790a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50b7e59dca01c5d06712",
+    "url": "/home/static/js/3.a13c895b.chunk.js"
+  },
+  {
+    "revision": "9c40c554372b3cc5588a",
+    "url": "/home/static/js/main.b2ac86b3.chunk.js"
+  },
+  {
+    "revision": "d43d23699e334b6f1f76",
+    "url": "/home/static/js/runtime-main.2ca16f47.js"
+  },
+  {
+    "revision": "34c68c57b8cedbf66c2ea14becfc3ff8",
+    "url": "/home/static/media/linkedin.34c68c57.png"
+  },
+  {
+    "revision": "adc1e143f81c0fc35edb0f004078e3a4",
+    "url": "/home/static/media/myraL.adc1e143.PNG"
+  },
+  {
+    "revision": "333b64cf5f4a76288bb2a7fe74def4d7",
+    "url": "/home/static/media/myraS.333b64cf.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.47a6c37e6b03908f9fd4af1a45b1a7d0.js"
+  "/home/precache-manifest.eae4dc43e2c7989f2c5dcbcfa725faeb.js"
 );
 
 self.addEventListener('message', (event) => {
